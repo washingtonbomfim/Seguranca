1a189f7bd2caf023ff0223a248d88f64cebf240a
washingtonbomfim <washington1bomfim@gmail.com>
2016-08-17 16:50:12 -0300

    sdas

--- www/Seguranca.js
+++ www/Seguranca.js
@@ -1,12 +1,12 @@
 var exec = require('cordova/exec');
 
-exports.Encrypt = function(arg0,arg1, success, error) {
+exports.Encrypt = function(arg0,arg1, callback, error) {
     exec(function(p) {
         callback(p);
     }, error, "Seguranca", "Encrypt", [arg0,arg1]);
 };
 
-exports.Decrypt = function(arg0,arg1, success, error) {
+exports.Decrypt = function(arg0,arg1, callback, error) {
     exec(function(p) {
         callback(p);
     }, error, "Seguranca", "Decrypt", [arg0,arg1]);
